2cc73567cd5329df74c76d6c31863230397d37b2
jessicab26 <brogdenjessica@gmail.com>
2022-01-27 09:59:33 -0600

    updated recipes.js to include api call for spoonacular.com/imported axios to project and created functions to handle search of recipes

--- recipes.js
+++ recipes.js
@@ -1,1 +1,19 @@
+function searchRecipe(recipe) {
+  const apiKey = "b5cf6a2382894ecdbbb832a0a89141ab";
+  let apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${recipe}&apiKey=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
+}
 
+function handleSubmit(event) {
+  event.preventDefault();
+  let recipe = document.querySelector("#change-recipe").value;
+  searchRecipe(recipe);
+}
+
+function handleResponse(response) {
+  console.log(response);
+  alert("hello");
+}
+
+let form = document.querySelector("#search-recipe");
+form.addEventListener("submit", handleSubmit);
