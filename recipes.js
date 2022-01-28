3981cc60e8ee519b40243a79e3b258a2685649b7
jessicab26 <brogdenjessica@gmail.com>
2022-01-27 20:19:22 -0600

    styled css for recipes.html page/updated recipes.js file using api call to display recipes images and titles/added div.row to recipes.html to add columns for recipes images

--- recipes.js
+++ recipes.js
@@ -1,6 +1,6 @@
 function searchRecipe(recipe) {
   const apiKey = "b5cf6a2382894ecdbbb832a0a89141ab";
-  let apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${recipe}&apiKey=${apiKey}`;
+  let apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${recipe}&apiKey=${apiKey}&diet=Vegan`;
   axios.get(apiUrl).then(handleResponse);
 }
 
@@ -12,7 +12,34 @@ function handleSubmit(event) {
 
 function handleResponse(response) {
   console.log(response);
-  alert("hello");
+  document.querySelector("#recipe-one").innerHTML =
+    response.data.results[0].title;
+
+  document.querySelector("#recipe-two").innerHTML =
+    response.data.results[1].title;
+  document.querySelector("#recipe-three").innerHTML =
+    response.data.results[2].title;
+  let main = document.querySelector("#main-dish");
+  let second = document.querySelector("#second-dish");
+  let third = document.querySelector("#third-dish");
+  main.setAttribute(
+    "src",
+    `https://spoonacular.com/recipeImages/${response.data.results[0].id}-312x231.jpg`
+  );
+  main.setAttribute("alt", response.data.results[0].title);
+  main.setAttribute("title", response.data.results[0].title);
+  second.setAttribute(
+    "src",
+    `https://spoonacular.com/recipeImages/${response.data.results[1].id}-312x231.jpg`
+  );
+  second.setAttribute("alt", response.data.results[1].title);
+  second.setAttribute("title", response.data.results[1].title);
+  third.setAttribute(
+    "src",
+    `https://spoonacular.com/recipeImages/${response.data.results[2].id}-312x231.jpg`
+  );
+  third.setAttribute("alt", response.data.results[2].title);
+  third.setAttribute("title", response.data.results[2].title);
 }
 
 let form = document.querySelector("#search-recipe");
